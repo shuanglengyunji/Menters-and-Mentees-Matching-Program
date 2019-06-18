2f304ff7af78354e849b9cc7ee30b36e7bd10307
shuanglengyunji <liuhan211211@gmail.com>
2019-06-18 15:38:35 +1000

    12/06/2019 added a tableview and connected it to table in the database

--- Menters-and-Mentees-Matching-Program/mainwindow.cpp
+++ Menters-and-Mentees-Matching-Program/mainwindow.cpp
@@ -6,9 +6,61 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // app apth
+    QString path = qApp->applicationDirPath();
+    qDebug() << "App Dir:" << path;
+
+    // tmp path
+    QString tmp_path = path + "/tmp";
+    qDebug() << "tmp Path" << tmp_path;
+    QDir().mkdir(tmp_path);
+
+    // database path
+    QString db_path = tmp_path + "/" + MY_DATA_BASE_NAME;
+    if(QFile().exists(db_path))
+    {
+        QFile().remove(db_path);
+        qDebug() << "Old database removed";
+    }
+    qDebug() << "Database Path:" << db_path;
+
+    // database demo path
+    QString db_demo_path = tmp_path + "/" + MY_DATABASE_DEMO_NAME;
+    qDebug() << "Database Demo Path:" << db_demo_path;
+
+    // init database
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    //db.setDatabaseName(db_path);
+    db.setDatabaseName(db_demo_path);
+    if (!db.open()) {
+        qDebug() << "Cannot open database";
+        QMessageBox::critical(nullptr, QObject::tr("Cannot open database"),
+            QObject::tr("Unable to establish a database connection.\n"), QMessageBox::Cancel);
+        return;
+    }
+    qDebug() << "Database Init Success";
+
+    model = new QSqlTableModel(this,db);
+    model->setTable("m_Mentors");
+    model->setEditStrategy(QSqlTableModel::OnManualSubmit);
+    model->select();
+
+    ui->tableView_mentors->setModel(model);
+    ui->tableView_mentors->resizeColumnsToContents();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionImport_Mentors_triggered()
+{
+    qDebug() << "Import Mentors data";
+}
+
+void MainWindow::on_actionImport_Mentees_triggered()
+{
+    qDebug() << "Import Mentees data";
 }
--- Menters-and-Mentees-Matching-Program/mainwindow.h
+++ Menters-and-Mentees-Matching-Program/mainwindow.h
@@ -2,6 +2,19 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDebug>
+#include <QMessageBox>
+#include <QString>
+#include <QDir>
+#include <QTableView>
+
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QSqlQuery>
+#include <QSqlTableModel>
+
+# define MY_DATA_BASE_NAME "db_mm.db"
+# define MY_DATABASE_DEMO_NAME "db_mm_demo.db"
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +28,18 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionImport_Mentors_triggered();
+
+    void on_actionImport_Mentees_triggered();
+
 private:
     Ui::MainWindow *ui;
+
+    QSqlDatabase db;
+
+    QSqlTableModel * model;
+    QTableView *view;
 };
 
 #endif // MAINWINDOW_H
