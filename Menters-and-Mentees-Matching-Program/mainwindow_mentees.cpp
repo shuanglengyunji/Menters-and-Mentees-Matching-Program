a758747aa038637da67056550482b4c35694fefd
shuanglengyunji <liuhan211211@gmail.com>
2019-06-21 11:39:11 +1000

    1. complete add / delete / submit /revert function on both of the tables 2. change the layout of the interface

--- Menters-and-Mentees-Matching-Program/mainwindow_mentees.cpp
+++ Menters-and-Mentees-Matching-Program/mainwindow_mentees.cpp
@@ -22,20 +22,63 @@ void MainWindow::on_lineEdit_mentees_search_editingFinished()
 
 void MainWindow::on_pushButton_mentees_add_clicked()
 {
+    QSqlTableModel *tm = qobject_cast<QSqlTableModel *>(ui->tableView_mentees->model());
+    if (!tm)
+        return;
 
+    QModelIndex insertIndex = ui->tableView_mentees->currentIndex();
+    int row = insertIndex.row() == -1 ? 0 : insertIndex.row();
+    tm->insertRow(row);
+    insertIndex = tm->index(row, 0);
+    ui->tableView_mentees->setCurrentIndex(insertIndex);
+    ui->tableView_mentees->edit(insertIndex);
+
+    qDebug() << "Insert Row";
 }
 
 void MainWindow::on_pushButton_mentees_delete_clicked()
 {
+    QSqlTableModel *tm = qobject_cast<QSqlTableModel *>(ui->tableView_mentees->model());
+    if (!tm)
+        return;
 
+    QModelIndexList currentSelection = ui->tableView_mentees->selectionModel()->selectedIndexes();
+    for (int i = 0; i < currentSelection.count(); ++i) {
+        tm->removeRow(currentSelection.at(i).row());
+    }
+
+    if (tm){
+        if(!tm->submitAll()){
+            qDebug()<<"Unable to delete"<<endl;
+            QSqlError err = tm->lastError();
+            QMessageBox::warning(this, tr("Unable to delete"), tr("An error occurred while "
+                                       "deleting the connection: ") + err.text());
+        }
+    }
+
+    qDebug() << "Delete Row";
 }
 
 void MainWindow::on_pushButton_mentees_revert_clicked()
 {
+    QSqlTableModel * tm = qobject_cast<QSqlTableModel *>(ui->tableView_mentees->model());
+    if (tm)
+        tm->revertAll();
 
+    qDebug() << "Revert";
 }
 
 void MainWindow::on_pushButton_mentees_submit_clicked()
 {
+    QSqlTableModel * tm = qobject_cast<QSqlTableModel *>(ui->tableView_mentees->model());
+    if (tm){
+        if(!tm->submitAll()){
+            qDebug()<<"Unable to submit"<<endl;
+            QSqlError err = tm->lastError();
+            QMessageBox::warning(this, tr("Unable to submit"), tr("An error occurred while "
+                                       "submitting the connection: ") + err.text());
+        }
+    }
 
+    qDebug() << "Submit All";
 }
--- Menters-and-Mentees-Matching-Program/mainwindow_mentors.cpp
+++ Menters-and-Mentees-Matching-Program/mainwindow_mentors.cpp
@@ -22,25 +22,63 @@ void MainWindow::on_lineEdit_mentors_search_editingFinished()
 
 void MainWindow::on_pushButton_mentors_add_clicked()
 {
-    int row = ui->tableView_mentors->selectionModel()->currentIndex().row();
-    model_mentors->insertRow(row);
+    QSqlTableModel *tm = qobject_cast<QSqlTableModel *>(ui->tableView_mentors->model());
+    if (!tm)
+        return;
+
+    QModelIndex insertIndex = ui->tableView_mentors->currentIndex();
+    int row = insertIndex.row() == -1 ? 0 : insertIndex.row();
+    tm->insertRow(row);
+    insertIndex = tm->index(row, 0);
+    ui->tableView_mentors->setCurrentIndex(insertIndex);
+    ui->tableView_mentors->edit(insertIndex);
+
     qDebug() << "Insert Row";
 }
 
 void MainWindow::on_pushButton_mentors_delete_clicked()
 {
-    int row = ui->tableView_mentors->selectionModel()->currentIndex().row();
-    model_mentors->removeRow(row);
-    model_mentors->submit();
+    QSqlTableModel *tm = qobject_cast<QSqlTableModel *>(ui->tableView_mentors->model());
+    if (!tm)
+        return;
+
+    QModelIndexList currentSelection = ui->tableView_mentors->selectionModel()->selectedIndexes();
+    for (int i = 0; i < currentSelection.count(); ++i) {
+        tm->removeRow(currentSelection.at(i).row());
+    }
+
+    if (tm){
+        if(!tm->submitAll()){
+            qDebug()<<"Unable to delete"<<endl;
+            QSqlError err = tm->lastError();
+            QMessageBox::warning(this, tr("Unable to delete"), tr("An error occurred while "
+                                       "deleting the connection: ") + err.text());
+        }
+    }
+
     qDebug() << "Delete Row";
 }
 
 void MainWindow::on_pushButton_mentors_revert_clicked()
 {
+    QSqlTableModel * tm = qobject_cast<QSqlTableModel *>(ui->tableView_mentors->model());
+    if (tm)
+        tm->revertAll();
 
+    qDebug() << "Revert";
 }
 
 void MainWindow::on_pushButton_mentors_submit_clicked()
 {
+    QSqlTableModel * tm = qobject_cast<QSqlTableModel *>(ui->tableView_mentors->model());
+    if (tm){
+        if(!tm->submitAll()){
+            qDebug()<<"Unable to submit"<<endl;
+            QSqlError err = tm->lastError();
+            QMessageBox::warning(this, tr("Unable to submit"), tr("An error occurred while "
+                                       "submitting the connection: ") + err.text());
+        }
+    }
 
+    qDebug() << "Submit All";
 }
