0914353dfc545521e5756447a2d3e6a2ef640a03
sukays <katerinawangsk@gmail.com>
2019-11-30 11:49:19 +1100

    Update set4anu_v2.sql

--- Database_Generate_Script/set4anu_v2.sql
+++ Database_Generate_Script/set4anu_v2.sql
@@ -1,54 +1,38 @@
-INSERT INTO sqlite_master (type, name, tbl_name, rootpage, sql) VALUES ('table', 'mentor', 'mentor', 5, 'CREATE TABLE "mentor"
-(
-	uid varchar(20) not null
-		constraint mentor_pk
-			primary key,
-	first_name varchar(30) not null,
-	last_name varchar(30) not null,
-	gender varchar(20) not null,
-	academic_level varchar(20) not null,
-	college varchar(50) not null,
-	degree varchar(50) not null,
-	type varchar(20) not null,
-	languages varchar(50) not null,
-	hall varchar(30) not null,
-	special text,
-	interests text,
-	wwvp varchar(30) not null,
-	train_1 char(1) default ''n'' not null,
-	train_2 char(1) default ''n'' not null,
-	train_3 char(1) default ''n'' not null,
-	train_complete char(1) default ''n'' not null,
-	round_1 char(1) default ''n'' not null,
-	round_2 char(1) default ''n'' not null
-)');
-INSERT INTO sqlite_master (type, name, tbl_name, rootpage, sql) VALUES ('index', 'sqlite_autoindex_mentor_1', 'mentor', 6, null);
-INSERT INTO sqlite_master (type, name, tbl_name, rootpage, sql) VALUES ('index', 'mentor_uid_uindex', 'mentor', 2, 'CREATE UNIQUE INDEX mentor_uid_uindex
-	on mentor (uid)');
-INSERT INTO sqlite_master (type, name, tbl_name, rootpage, sql) VALUES ('table', 'mentee', 'mentee', 3, 'CREATE TABLE mentee
-(
-	uid varchar(10) not null
-		constraint mentee_pk
-			primary key,
-	first_name varchar(20) not null,
-	last_name varchar(20) not null,
-	academic_level varchar(10) not null,
-	college varchar(50) not null,
-	requests text,
-	round_1 char(1) default ''n'' not null,
-	round_2 char(1) default ''n'' not null
-)');
-INSERT INTO sqlite_master (type, name, tbl_name, rootpage, sql) VALUES ('index', 'sqlite_autoindex_mentee_1', 'mentee', 4, null);
-INSERT INTO sqlite_master (type, name, tbl_name, rootpage, sql) VALUES ('index', 'mentee_uid_uindex', 'mentee', 7, 'CREATE UNIQUE INDEX mentee_uid_uindex
-	on mentee (uid)');
-INSERT INTO sqlite_master (type, name, tbl_name, rootpage, sql) VALUES ('table', 'match', 'match', 8, 'CREATE TABLE match
-(
-	m_id integer not null
-		constraint match_pk
-			primary key autoincrement,
-	mentor_id varchar(20) not null,
-	mentee_id varchar(20) not null
-)');
-INSERT INTO sqlite_master (type, name, tbl_name, rootpage, sql) VALUES ('table', 'sqlite_sequence', 'sqlite_sequence', 9, 'CREATE TABLE sqlite_sequence(name,seq)');
-INSERT INTO sqlite_master (type, name, tbl_name, rootpage, sql) VALUES ('index', 'match_m_id_uindex', 'match', 10, 'CREATE UNIQUE INDEX match_m_id_uindex
-	on match (m_id)');
+CREATE TABLE "match" (
+	"mid" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE, 
+	"mentor_id" VARCHAR (10) NOT NULL, 
+	"mentee_id" VARCHAR (10) NOT NULL
+	);
+
+CREATE TABLE "mentee" (
+	"uid" VARCHAR (10) PRIMARY KEY NOT NULL UNIQUE, 
+	"first_name" VARCHAR (20) NOT NULL, 
+	"last_name" VARCHAR (20) NOT NULL, 
+	"academic_level" VARCHAR (20) NOT NULL, 
+	"college" VARCHAR (50) NOT NULL, 
+	"requests" TEXT (500), 
+	"round_1" CHAR (1) NOT NULL, 
+	"round_2" CHAR (1) NOT NULL
+	);
+
+CREATE TABLE "mentor" (
+	"uid" VARCHAR (10) PRIMARY KEY NOT NULL UNIQUE, 
+	"first_name" VARCHAR (20) NOT NULL, 
+	"last_name" VARCHAR (20) NOT NULL, 
+	"gender" VARCHAR (20) NOT NULL, 
+	"academic_level" VARCHAR (20) NOT NULL, 
+	"college" VARCHAR (50) NOT NULL, 
+	"degree" VARCHAR (30) NOT NULL, 
+	"type" VARCHAR (20) NOT NULL, 
+	"languages" VARCHAR (20) NOT NULL, 
+	"hall" VARCHAR (20) NOT NULL, 
+	"special" TEXT (500), 
+	"interests" TEXT (500), 
+	"wwvp" VARCHAR (10) NOT NULL, 
+	"train_1" CHAR (1) NOT NULL, 
+	"train_2" CHAR (1) NOT NULL, 
+	"train_3" CHAR (1) NOT NULL, 
+	"train_complete" CHAR (1) NOT NULL, 
+	"round_1" CHAR (1) NOT NULL, 
+	"round_2" CHAR (1) NOT NULL
+	);
